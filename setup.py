d69b01e305c96b752e7ba2bb0b1ba7418286bd41
Merlintor <merlinfuchs2001@gmail.com>
2018-10-01 15:23:36 +0200

    pip install

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='discord_backups',
+    version='0.1.0',
+    description='Create backups of your discord server',
+    long_description=readme,
+    author='Merlin Fuchs (Merlintor)',
+    author_email='merlinfuchs2001@gmail.com',
+    url='https://github.com/Merlintor/discord-backups',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
